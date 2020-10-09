186fccc99781a24c8a0723ee07d860a1b54ba0c4
Nikita Miazin <myazin.n@list.ru>
2020-10-09 11:11:07 +0300

    Add simplification from `layer.build.use` to `zio.provideLayer`

--- src/main/scala/zio/intellij/utils/TypeCheckUtils.scala
+++ src/main/scala/zio/intellij/utils/TypeCheckUtils.scala
@@ -3,15 +3,16 @@ package zio.intellij.utils
 import org.jetbrains.plugins.scala.codeInspection.collections.isOfClassFrom
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
 import org.jetbrains.plugins.scala.lang.psi.types.ScType
-import zio.intellij.utils.types.ZioTypes
+import zio.intellij.utils.types.{ZLayerTypes, ZioTypes}
 
 object TypeCheckUtils {
 
   val zioTypes        = ZioTypes.values.map(_.fqName).toArray
+  val zioLayerTypes   = ZLayerTypes.values.map(_.fqName).toArray
   val managedTypes    = Array("zio.ZManaged")
   val extraTypes      = Array("zio.Fiber", "zio.ZQueue", "zio.ZRef", "zio.ZRefM", "zio.ZQuery")
   val zioTest         = Array("zio.test._")
-  val zioLikePackages = zioTypes ++ managedTypes ++ extraTypes ++ zioTest
+  val zioLikePackages = zioTypes ++ zioLayerTypes ++ managedTypes ++ extraTypes ++ zioTest
 
   def fromZioLike(r: ScExpression): Boolean =
     isOfClassFrom(r, zioLikePackages)
